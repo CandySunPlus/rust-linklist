8137988d3fa9009c47abcc36d8d4f8c362c8a27d
sunbeiluo <sunbeiluo@meituan.com>
2024-07-31 17:37:19 +0800

    add unsafe deque

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod first;
 pub mod five;
 pub mod four;
 pub mod second;
+pub mod six;
 pub mod third;
---
+++ src/six.rs
@@ -0,0 +1,110 @@
+use std::ptr::NonNull;
+
+#[derive(Default)]
+pub struct LinkedList<T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+}
+
+type Link<T> = Option<NonNull<Node<T>>>;
+
+struct Node<T> {
+    front: Link<T>,
+    back: Link<T>,
+    elem: T,
+}
+
+impl<T> LinkedList<T> {
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            let mut new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+
+            if let Some(old) = self.front.as_mut() {
+                old.as_mut().front = Some(new);
+                new.as_mut().back = Some(*old);
+            } else {
+                debug_assert!(self.back.is_none());
+                debug_assert!(self.front.is_none());
+                debug_assert!(self.len == 0);
+                self.back = Some(new);
+            }
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            self.front.map(|node| {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let elem = boxed_node.elem;
+
+                self.front = boxed_node.back;
+
+                if let Some(new) = self.front.as_mut() {
+                    new.as_mut().front = None;
+                } else {
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+
+                self.len -= 1;
+                elem
+            })
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_basic_front() {
+        let mut list = LinkedList::default();
+
+        // Try to break an empty list
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Try to break a one item list
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Mess around
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        list.push_front(20);
+        assert_eq!(list.len(), 2);
+        list.push_front(30);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(30));
+        assert_eq!(list.len(), 2);
+        list.push_front(40);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(40));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop_front(), Some(20));
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+    }
+}
